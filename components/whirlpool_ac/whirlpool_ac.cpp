311064615101907bd93a3be9ebb15f2f502992df
d1mdev <109150637+d1mdev@users.noreply.github.com>
2023-05-10 00:23:54 +0300

    Update whirlpool_ac.cpp
    ...

--- components/whirlpool_ac/whirlpool_ac.cpp
+++ components/whirlpool_ac/whirlpool_ac.cpp
@@ -149,7 +149,7 @@ void WhirlpoolClimateAC::transmit_state() {
   ::time(&t_transmit);
   //this->t_transmit = this->timestamp_now();
   //ESP_LOGD(TAG, "Transmitter mute state is %02X", esphome::binary_sensor::BinarySensor(climate_ir_muted).state);
-  if (this->ir_sensor_ != nullptr && this->ir_sensor_->state == "On") {
+  if (this->ir_sensor_ != nullptr && this->ir_sensor_->state) {
      transmit.perform();
   }
 }
