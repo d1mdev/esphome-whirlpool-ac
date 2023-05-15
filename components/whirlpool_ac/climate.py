c1bcfec15ce73b920922d2a52b5d00fa649b8e3a
d1mdev <109150637+d1mdev@users.noreply.github.com>
2023-05-15 22:16:35 +0300

    Update climate.py
    ...

--- components/whirlpool_ac/climate.py
+++ components/whirlpool_ac/climate.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate_ir, sensor
-from esphome.const import CONF_ID, CONF_MODEL
+from esphome.const import CONF_ID, CONF_MODEL, STATE_CLASS_MEASUREMENT
 
 AUTO_LOAD = ["climate_ir", "sensor"]
 CODEOWNERS = ["@glmnet"]
