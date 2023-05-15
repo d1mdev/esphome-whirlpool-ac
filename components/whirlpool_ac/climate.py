7ae9618a625e4154d7aa366b0638236c6b240a4a
d1mdev <109150637+d1mdev@users.noreply.github.com>
2023-05-15 22:33:03 +0300

    Update climate.py
    ...

--- components/whirlpool_ac/climate.py
+++ components/whirlpool_ac/climate.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import climate_ir, sensor
+from esphome.components import climate_ir, sensor, homeassistant::HomeassistantBinarySensor
 from esphome.const import CONF_ID, CONF_MODEL, STATE_CLASS_MEASUREMENT
 
 AUTO_LOAD = ["climate_ir", "sensor"]
@@ -21,7 +21,7 @@ CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(WhirlpoolClimateAC),
         cv.Optional(CONF_MODEL, default="DG11J1-3A"): cv.enum(MODELS, upper=True),
-        cv.Optional(CONF_IR_TRANSMITTER_MUTE): cv.use_id(sensor.Sensor),
+        cv.Optional(CONF_IR_TRANSMITTER_MUTE): cv.use_id(sensor.homeassistant::HomeassistantBinarySensor),
     }
 )
 
