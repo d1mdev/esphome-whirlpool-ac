9d70126e20ed3ea748d5665d5b1be4c37a604d00
d1mdev <109150637+d1mdev@users.noreply.github.com>
2024-03-31 17:50:45 +0300

    ...

--- components/whirlpool_ac/whirlpool_ac.cpp
+++ components/whirlpool_ac/whirlpool_ac.cpp
@@ -106,16 +106,16 @@ void WhirlpoolClimateAC::transmit_state() {
   }
   
   // Preset for iFeel
-/*   switch (this->preset.value()) {
+  switch (this->preset.value()) {
     case climate::CLIMATE_PRESET_NONE:
-      ESP_LOGD(TAG, "Preset: %s", this->preset);
+      ESP_LOGD(TAG, "Preset: NONE");
       break;
     case climate::CLIMATE_PRESET_ACTIVITY:
-      ESP_LOGD(TAG, "Preset: %s", this->preset);
+      ESP_LOGD(TAG, "Preset: ACTIVITY");
       break;
     default:
       break;
-  } */
+  }
 
   // Checksum
   for (uint8_t i = 2; i < 13; i++)
